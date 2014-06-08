b1c549b360f4fb62d76074252591a5daf8f3c35c
Mike Cooper <mythmon@gmail.com>
2014-06-08 11:51:25 -0700

    Basic solver. Resizer. Tweaks.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,16 @@
-import {makePuzzleMesh} from "./cycles";
-
-var WIDTH = 700;
-var HEIGHT = 700;
-var MARGIN = 25;
+import {makePuzzleMesh, oneSolutionStep} from "./cycles";
 
 var puzzleMesh = makePuzzleMesh();
 
-var svg = d3.select('body').append('svg')
-  .attr('width', WIDTH)
-  .attr('height', HEIGHT);
+var svg = d3.select('body').append('svg');
 
 var xscale = d3.scale.linear()
   .domain(d3.extent(puzzleMesh.verts, (d) => d.x))
-  .range([MARGIN, WIDTH - MARGIN]);
-var xscaleZero = xscale.copy().range([0, WIDTH - MARGIN * 2]);
+var xscaleZero = xscale.copy();
 
 var yscale = d3.scale.linear()
   .domain(d3.extent(puzzleMesh.verts, (d) => d.y))
-  .range([MARGIN, HEIGHT - MARGIN]);
-var yscaleZero = yscale.copy().range([0, HEIGHT - MARGIN * 2]);
+var yscaleZero = yscale.copy();
 
 var lineOpen = d3.svg.line()
     .x((v) => xscale(v.x))
@@ -31,30 +23,51 @@ var elems = {
     faceNums: svg.append('g').classed('face-nums', true),
     edges: svg.append('g').classed('edges', true),
     verts: svg.append('g').classed('verts', true),
-    debug: svg.append('g').classed('debug', true),
+    voronoi: svg.append('g').classed('voronois', true),
 };
+
+function rescaleGraph() {
+  var size;
+  if (window.innerWidth > window.innerHeight) {
+    size = window.innerHeight * 0.9;
+  } else {
+    size = window.innerWidth * 0.9;
+  }
+  var margin = size * 0.1;
+
+  xscale.range([margin, size - margin]);
+  yscale.range([margin, size - margin]);
+  xscaleZero.range([0, size - 2 * margin]);
+  yscaleZero.range([0, size - 2 * margin]);
+  svg
+    .attr('width', size)
+    .attr('height', size);
+  update();
+}
+rescaleGraph();
+window.onresize = rescaleGraph;
 
 function update() {
     // Join
     var faces = elems.faces.selectAll('.face').data(puzzleMesh.faces, (f) => f.id);
-    var faceNums = elems.faceNums.selectAll('.faceNum').data(puzzleMesh.faces, (f) => f.id);
+    var faceNums = elems.faceNums.selectAll('.face-num').data(puzzleMesh.faces, (f) => 'n' + f.id);
     var edges = elems.edges.selectAll('.edge').data(puzzleMesh.edges, (e) => e.id);
     var verts = elems.verts.selectAll('.vert').data(puzzleMesh.verts, (v) => v.id);
-    var debug = elems.debug.selectAll('.voronoi').data(puzzleMesh.voronoi());
+    var voronoi = elems.voronoi.selectAll('.voronoi').data(puzzleMesh.voronoi());
 
     // Enter
     faces.enter().append('path').classed('face', true);
     faceNums.enter().append('text').classed('face-num', true);
     edges.enter().append('path').classed('edge', true);
     verts.enter().append('circle').classed('vert', true);
-    debug.enter().append('path').classed('voronoi', true);
+    voronoi.enter().append('path').classed('voronoi', true);
 
     // Update
     faces.attr('d', (f) => lineClosed(f.verts()));
     faceNums
       .attr('x', (f) => xscale(f.center().x))
       .attr('y', (f) => yscale(f.center().y))
-      .text((f) => f.edgeActiveCount)
+      .text((f) => f.hint)
       .style('font-size', (f) => yscaleZero(0.6) + 'px');
     edges
       .attr('d', (e) => lineOpen(e.verts()))
@@ -63,12 +76,7 @@ function update() {
       .attr('cx', (v) => xscale(v.x))
       .attr('cy', (v) => yscale(v.y))
       .attr('r', 3);
-    debug
-      // .attr('stroke', 'rgba(255,0,0,0.5)')
-      // .attr('fill', (v) => v.point.type === 'edge' ? edgeColorScale(v.point.edge.state) : '#248')
-      // .attr('fill', 'none')
-      // .attr('stroke', 'none')
-      // .style('fill-events', 'all')
+    voronoi
       .attr('d', (poly) => 'M' + poly.map((p) => [xscale(p[0]), yscale(p[1])]).join('L'))
       .datum((d) => d.point)
       .on('click', voronoiOnClick)
@@ -79,17 +87,9 @@ function update() {
     faceNums.exit().remove();
     edges.exit().remove();
     verts.exit().remove();
-    debug.exit().remove();
+    voronoi.exit().remove();
 }
 
-
-function anim() {
-    for (var v of puzzleMesh.verts) {
-        v.x += Math.random() * 0.02 - 0.01;
-        v.y += Math.random() * 0.02 - 0.01;
-    }
-    update();
-}
 
 function voronoiOnClick(d) {
   var edgeStateTransitions = {true: false, false: 'none', 'none': true};
@@ -99,5 +99,16 @@ function voronoiOnClick(d) {
   update();
 }
 
+function nextSolutionStep() {
+  var madeChange = oneSolutionStep(puzzleMesh);
+  update();
+  if (!madeChange) {
+    alert("I can't solve anything more.")
+    // clearInterval(solvingInterval);
+  }
+}
+
+d3.select('body').append('button').text('Step Solve').on('click', nextSolutionStep);
+
 update();
-// d3.timer(anim);
+// var solvingInterval = setInterval(nextSolutionStep, 1000);
--- src/cycles.js
+++ src/cycles.js
@@ -1,48 +1,155 @@
 import {Mesh} from "./mesh";
 
-var loopNodes = [
-    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [5, 1], [6, 1], [6, 2],
-    [7, 2], [7, 1], [7, 0], [8, 0], [8, 1], [8, 2], [8, 3], [7, 3], [6, 3],
-    [6, 4], [5, 4], [5, 3], [4, 3], [4, 4], [4, 5], [5, 5], [6, 5], [6, 6],
-    [5, 6], [4, 6], [4, 7], [5, 7], [6, 7], [7, 7], [7, 6], [8, 6], [8, 5],
-    [7, 5], [7, 4], [8, 4], [9, 4], [9, 3], [9, 2], [9, 1], [9, 0], [10, 0],
-    [10, 1], [10, 2], [10, 3], [10, 4], [10, 5], [9, 5], [9, 6], [10, 6],
-    [10, 7], [9, 7], [8, 7], [8, 8], [7, 8], [6, 8], [6, 9], [7, 9], [8, 9],
-    [9, 9], [10, 9], [10, 10], [9, 10], [8, 10], [7, 10], [6, 10], [5, 10],
-    [5, 9], [4, 9], [4, 10], [3, 10], [2, 10], [2, 9], [3, 9], [3, 8], [2, 8],
-    [1, 8], [1, 9], [1, 10], [0, 10], [0, 9], [0, 8], [0, 7], [1, 7], [2, 7],
-    [3, 7], [3, 6], [3, 5], [2, 5], [1, 5], [1, 6], [0, 6], [0, 5], [0, 4],
-    [1, 4], [2, 4], [3, 4], [3, 3], [2, 3], [2, 2], [3, 2], [4, 2], [4, 1],
-    [3, 1], [2, 1], [1, 1], [1, 2], [1, 3], [0, 3], [0, 2], [0, 1]];
+var PUZZLE_SIZE = 4;
+// var loopNodes = [
+//     [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [5, 1], [6, 1], [6, 2],
+//     [7, 2], [7, 1], [7, 0], [8, 0], [8, 1], [8, 2], [8, 3], [7, 3], [6, 3],
+//     [6, 4], [5, 4], [5, 3], [4, 3], [4, 4], [4, 5], [5, 5], [6, 5], [6, 6],
+//     [5, 6], [4, 6], [4, 7], [5, 7], [6, 7], [7, 7], [7, 6], [8, 6], [8, 5],
+//     [7, 5], [7, 4], [8, 4], [9, 4], [9, 3], [9, 2], [9, 1], [9, 0], [10, 0],
+//     [10, 1], [10, 2], [10, 3], [10, 4], [10, 5], [9, 5], [9, 6], [10, 6],
+//     [10, 7], [9, 7], [8, 7], [8, 8], [7, 8], [6, 8], [6, 9], [7, 9], [8, 9],
+//     [9, 9], [10, 9], [10, 10], [9, 10], [8, 10], [7, 10], [6, 10], [5, 10],
+//     [5, 9], [4, 9], [4, 10], [3, 10], [2, 10], [2, 9], [3, 9], [3, 8], [2, 8],
+//     [1, 8], [1, 9], [1, 10], [0, 10], [0, 9], [0, 8], [0, 7], [1, 7], [2, 7],
+//     [3, 7], [3, 6], [3, 5], [2, 5], [1, 5], [1, 6], [0, 6], [0, 5], [0, 4],
+//     [1, 4], [2, 4], [3, 4], [3, 3], [2, 3], [2, 2], [3, 2], [4, 2], [4, 1],
+//     [3, 1], [2, 1], [1, 1], [1, 2], [1, 3], [0, 3], [0, 2], [0, 1]];
+var loopNodes = [[2, 0], [3, 0], [4, 0], [4, 1], [4, 2], [4, 3], [3, 3], [3, 2],
+[2, 2], [2, 3], [1, 3], [0, 3], [0, 2], [1, 2], [1, 1], [2, 1]];
 
 function square(x, y) {
-    return [[x, y], [x + 1, y], [x + 1, y + 1], [x, y + 1]];
+  return [[x, y], [x + 1, y], [x + 1, y + 1], [x, y + 1]];
 }
 
 export function makePuzzleMesh() {
-    var mesh = new Mesh();
-    var e, v1, v2;
+  var mesh = new Mesh();
+  var e, v1, v2;
 
-    for (var x = 0; x < 10; x++) {
-        for (var y = 0; y < 10; y++) {
-            var f = mesh.addFace(...square(x, y));
+  var hints = [0, 2, null, null, null, null, null, 1, 3, null, null, 3, null, null, 0, null];
+  for (var x = 0; x < PUZZLE_SIZE; x++) {
+    for (var y = 0; y < PUZZLE_SIZE; y++) {
+      var f = mesh.addFace(...square(x, y));
+      f.hint = hints[y * 4 + x];
+    }
+  }
+
+  for (e of mesh.edges) {
+    e.state = false;
+  }
+
+  for (var i = 0; i < loopNodes.length; i++) {
+    v1 = loopNodes[i];
+    v2 = loopNodes[(i + 1) % loopNodes.length];
+    e = mesh.getEdgeFrom(v1, v2);
+    e.state = true;
+  }
+
+  // for (f of mesh.faces) {
+  //   f.hint = f.edges.filter((f) => f.state).length;
+  // }
+
+  for (e of mesh.edges) {
+    e.state = 'none';
+  }
+
+  return mesh;
+}
+
+export function oneSolutionStep(puzzle) {
+  for (var i = 0; i < solvingMethods.length; i++) {
+    var ret = solvingMethods[i](puzzle);
+    if (ret) {
+      console.log('Stepped using strategy: ', solvingMethods[i].name);
+      return true;
+    }
+  }
+  return false;
+}
+
+/* Ever solving method in this should find one piece of evidence, make changes
+* to the puzzle based on only that piece of evidence, and then return true.
+* If no evidence it found, and thus no changes made, it should return false.
+*/
+var solvingMethods = [singleVertPatterns, fillNums, emptyNums, facesInCorners];
+
+function _getUnsetEdge(d) {
+  var unsetEdges = d.edges.filter((e) => e.state === 'none');
+  if (unsetEdges.length === 0) {
+    console.error('ineligible object to setOn:', d);
+    throw new Error('Logic error, tried to set/clear on ineligible object.');
+  }
+  return unsetEdges[0];
+}
+function _setOneEdge(d) {
+  _getUnsetEdge(d).state = true;
+  return true;
+}
+function _clearOneEdge(d) {
+  _getUnsetEdge(d).state = false;
+  return true;
+}
+function _componentCounts(d) {
+  var a = d.edges.length;
+  var t = d.edges.filter((e) => e.state === true).length;
+  var f = d.edges.filter((e) => e.state === false).length;
+  var n = d.edges.filter((e) => e.state === 'none').length;
+  return {a, t, f, n};
+}
+
+function singleVertPatterns(puzzle) {
+  for (var v of puzzle.verts) {
+    var {a, t, f, n} = _componentCounts(v);
+    if (t + f + n !== a) {
+      throw new Error('Algorithm error, n+t+f != a');
+    }
+
+    if (f === a - 1 && n > 0) {
+      return _clearOneEdge(v);
+    }
+    if (n === 1 && t === 1) {
+      return _setOneEdge(v);
+    }
+    if (t === 2 && n > 0) {
+      return _clearOneEdge(v);
+    }
+  }
+  return false;
+}
+
+function fillNums(puzzle) {
+  for (var face of puzzle.faces.filter((f) => f.hint !== null)) {
+    var {a, t, f, n} = _componentCounts(face);
+    if (t > face.hint) {
+      throw new Error("Logic error, face with too many edges set.");
+    }
+    if (f === a - face.hint && n > 0) {
+      return _setOneEdge(face);
+    }
+  }
+}
+
+function emptyNums(puzzle) {
+  for (var face of puzzle.faces.filter((f) => f.hint !== null)) {
+    var {a, t, f, n} = _componentCounts(face);
+    if (t === face.hint && n > 0) {
+      return _clearOneEdge(face);
+    }
+  }
+}
+
+function facesInCorners(puzzle) {
+  for (var face of puzzle.faces.filter((f) => f.hint !== null)) {
+    var faceCounts = _componentCounts(face);
+    // will marking two "n" edges as "f" make this face invalid?
+    if (faceCounts.a - faceCounts.t - faceCounts.n < face.hint) {
+      // Do any verts on this face have 2 "n" edges and no "t" edges.
+      for (var vert of face.verts()) {
+        var vertCounts = _componentCounts(vert);
+        if (vertCounts.t === 0 && vertCounts.n === 2) {
+          return _setOneEdge(vert);
         }
+      }
     }
-
-    for (e of mesh.edges) {
-        e.state = false;
-    }
-
-    for (var i = 0; i < loopNodes.length; i++) {
-        v1 = loopNodes[i];
-        v2 = loopNodes[(i + 1) % loopNodes.length];
-        e = mesh.getEdgeFrom(v1, v2);
-        e.state = true;
-    }
-
-    for (f of mesh.faces) {
-        f.edgeActiveCount = f.edges.filter((f) => f.state).length;
-    }
-
-    return mesh;
+  }
 }

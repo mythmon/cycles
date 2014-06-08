1316c9c22cb1e8a1c1eaf1f62f462246a1c00a02
Mike Cooper <mythmon@gmail.com>
2014-06-08 12:12:40 -0700

    Gulp better. GH pages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 // Include gulp
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
-
-// Watch Files For Changes
-gulp.task('watch', ['compile'], function() {
-  gulp.watch('src/*.js', ['compile']);
-});
+var htmlSrc = require('gulp-html-src');
+var ghPages = require("gulp-gh-pages");
 
 function errors(err) {
   this.end();
   console.error(err.toString());
 }
 
-gulp.task('compile', function () {
-  return gulp.src('src/*.js').on('error', errors)
-  .pipe(traceur({modules: 'instantiate', sourceMap: true})).on('error', errors)
-  .pipe(gulp.dest('dist')).on('error', errors);
+// Watch Files For Changes
+gulp.task('watch', ['build'], function() {
+  gulp.watch('src/*.js', ['build']);
+});
+
+gulp.task('build', function () {
+  gulp.src('index.html')
+    .pipe(htmlSrc())
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/**/*.js')
+    .pipe(traceur({modules: 'instantiate'})).on('error', errors)
+    .pipe(gulp.dest('dist/src'));
+  gulp.src('{index.html,style.css}')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', ['build'], function() {
+  gulp.src('dist/**/*')
+    .pipe(ghPages());
 });
 
 // Default Task
-gulp.task('default', ['compile']);
+gulp.task('default', ['buld']);

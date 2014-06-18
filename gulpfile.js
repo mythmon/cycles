19eb6ec246b63a30437714b25a3b045cbb70b4c2
Mike Cooper <mythmon@gmail.com>
2014-06-17 17:53:50 -0700

    Fix Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,56 @@ var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 var htmlSrc = require('gulp-html-src');
 var ghPages = require("gulp-gh-pages");
+var browserify = require('gulp-browserify');
+var rm = require('gulp-rm');
 
 function errors(err) {
   this.end();
   console.error(err.toString());
 }
 
-// Watch Files For Changes
-gulp.task('watch', ['build'], function() {
+gulp.task('default', ['build']);
+
+gulp.task('watch', function() {
   gulp.watch('src/*.js', ['build']);
 });
 
-gulp.task('build', function () {
-  gulp.src('index.html')
+gulp.task('build', [
+  'build.copy',
+  'build.traceur',
+  'build.browserify',
+  'build.vendor',
+]);
+
+gulp.task('build.copy', function() {
+  return gulp.src(['index.html', 'style.css'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build.vendor', function() {
+  return gulp.src('index.html')
     .pipe(htmlSrc())
     .pipe(gulp.dest('dist'));
-  gulp.src('src/**/*.js')
-    .pipe(traceur({modules: 'instantiate'})).on('error', errors)
-    .pipe(gulp.dest('dist/src'));
-  gulp.src('{index.html,style.css}')
+});
+
+gulp.task('build.traceur', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(traceur({modules: 'commonjs'})).on('error', errors)
+    .pipe(gulp.dest('build/src'));
+});
+
+gulp.task('build.browserify', ['build.traceur'], function() {
+  return gulp.src('build/src/app.js')
+    .pipe(browserify({debug: true}))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('deploy', ['build'], function() {
-  gulp.src('dist/**/*')
+  return gulp.src('dist/**/*')
     .pipe(ghPages());
 });
 
-// Default Task
-gulp.task('default', ['build']);
+gulp.task('clean', function() {
+  return gulp.src(['dist/**/*', 'build/**/*'])
+    .pipe(rm());
+});
